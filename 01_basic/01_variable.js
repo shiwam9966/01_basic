51addd02da59ab178a07fc8829f476ec13a23614
Shiwam Puri <purishiwam6@gmail.com>
2026-07-18 17:48:44 +0000

    done 1

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 112233
+let accountEmail = "purishiwam5@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+//accountid = 2// (not allowed)
+
+accountEmail = "shiv21@gmail.com" 
+accountPassword = "212121" 
+accountCity = "udaipur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
